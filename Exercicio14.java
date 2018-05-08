f5c6785f919bd14d4b902499e64d5b3ff84a80ff
Osmar Fagundes <36849593+osmarft@users.noreply.github.com>
2018-05-08 17:10:49 -0300

    Add files via upload

---
+++ Exercicio14.java
@@ -0,0 +1,32 @@
+package lista_2_estrutura_de_decisao;
+public class Exercicio14 {
+   private int horasTrabalhadas;
+   private Double valorPorHoraTrabalhada;
+
+    public Exercicio14() {
+    }
+
+    public Exercicio14(int horasTrabalhadas, Double valoHorasTrabalhadas) {
+        setHorasTrabalhadas(horasTrabalhadas);
+        setValorPorHoraTrabalhada(valoHorasTrabalhadas);
+    }
+
+    public int getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public Double getValorPorHoraTrabalhada() {
+        return valorPorHoraTrabalhada;
+    }
+
+    public void setValorPorHoraTrabalhada(Double valorPorHoraTrabalhada) {
+        this.valorPorHoraTrabalhada = valorPorHoraTrabalhada;
+    }
+   
+    
+   
+}
